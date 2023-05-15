49aa2be8dd294829f3895d88e1d7c3e2f3e6eb52
AnnaStandridge <annanstandridge@gmail.com>
2023-05-15 09:17:10 -0400

    Correction to error class 2

--- scripts/validation.js
+++ scripts/validation.js
@@ -62,7 +62,7 @@ const options = {
   inputSelector: ".modal__form_input",
   submitButtonSelector: ".modal__button",
   inactiveButtonClass: "modal__button_disabled",
-  inputErrorClass: "modal__type_error",
+  inputErrorClass: "modal__error",
   errorClass: "modal__error_visible",
 };
 
